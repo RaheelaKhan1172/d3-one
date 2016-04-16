df159cee31034dce596b111a31da4018eaf5a9b6
Raheela Khan <raheelak20@gmail.com>
2016-04-16 16:55:07 -0700

    data dispaly done tooltip implementation left

---
+++ d.js
@@ -0,0 +1,141 @@
+(function() {
+  var q = d3_queue.queue();
+  console.info(q);
+  q.defer(d3.json,'./GDP-data.json');
+  console.info(q);
+  q.await(fixData);
+  
+    function fixData(error, data) {
+        if (error) throw error;
+        
+        var dataToDisplay = {name: data.source_name, data:[]};
+        
+        
+        //closure
+        var parseDate = d3.time.format("%Y-%m-%d").parse;
+        
+        data.data.map((a,i) => {
+            return dataToDisplay.data.push({
+                value: a[1],
+                date: parseDate(a[0])
+            });
+        });
+        makeMap(dataToDisplay);
+    };
+
+  /** cb for results 
+    @return{null}
+  **/
+
+  function makeMap(dataToDisplay) {
+
+      var data = dataToDisplay.data;
+      
+      var timeExtent = d3.extent(data,function(d) {return d.date;});
+      
+
+      var margin = {top: 20,right:20, bottom:30, left:50, width:960, height:500};
+        margin.width = margin.width - margin.left - margin.right;
+        margin.height = margin.height - margin.top  - margin.bottom;
+     
+      
+      /************************** TOOLTIP **************/
+      var div = d3.select(".d3stuff").append("div")
+                  .attr("class", "tooltip")
+                  .style("display", "none");
+      
+      function mouseOver() {
+          div.style("display", "inline");
+      }
+      
+      function mouseMove(x) {
+          
+         var div = d3.select('.tooltip');
+            div.text('event')
+            .style("left", (d3.event.pageX-34) +"px")
+              .style("top", (d3.event.pageY - 12) + "px");
+                   
+     
+      }
+      
+      function mouseOut() {
+          div.style("display", "none");
+      }
+      
+      
+      /************** END TOOLTIP ********************/
+      
+      /********** CLOSURES *****/
+      
+      var x = d3.time.scale()
+                .range([0,margin.width]);
+      
+      var y = d3.scale.linear()
+                .range([margin.height,0]);
+          
+      
+      
+      var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom");
+      
+      var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .orient("left");
+      
+      //fix this
+      var area = d3.svg.area()
+                   .x(function(d) {return x(d.date);})
+                   .y0(margin.height)
+                   .y1(function(d) {return y(d.value);});
+      
+      /******** CLOSURES END ********/
+      
+      
+      
+    /***************************************** GRAPH ******************************/  
+    var svg = d3.select(".d3stuff").append("svg")
+                .attr("width", margin.width + margin.left + margin.right)
+                .attr("height", margin.height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      
+     
+      x.domain(timeExtent);
+      y.domain([0,d3.max(data,function(d) {return d.value;})]);
+      
+      svg.append("path")
+         .datum(data)
+         .attr("class", "area")
+         .attr("d", area)
+         .style("fill", "blue")
+          .on("mouseover", mouseOver)
+          .on("mousemove", mouseMove)
+          .on("mouseout", mouseOut);
+      
+      svg.append("g")
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + margin.height + ")")
+         .call(xAxis)
+         .style("fill", "pink");
+      
+      svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Price($)")
+        .style("fill", "red");
+      
+        /*********************************** END GRAPH **************************************/
+  }   
+    
+  // margin object
+    
+    
+
+})();
+
